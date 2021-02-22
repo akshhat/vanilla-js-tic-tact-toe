96417abfadde934ed065962e2693853b10463892
Akshhat <akshhat.srivastava@gmail.com>
2021-02-22 11:29:48 +0530

    Add working game play

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 const board = document.querySelector('main');
-const currentPlay = document.querySelector('#current-play');
+const currentPlayDisplay = document.querySelector('#current-play');
 const reset = document.querySelector('button');
 const count = 3;
 let cell_id = 1;
 let row_id = 1;
+let currentPlayer = 1;
+let currentMove = 0;
+
+// Winning logic
+let ticTacToe = new Array(9);
+const calcWinner = () => {
+}
+
+// Moves counter Function
+const playerMoves = () => {
+    currentMove++;
+    if (currentMove >= 4) {
+        calcWinner();
+    }
+    if (currentMove >= 9) {
+        currentPlayDisplay.innerText = 'Game Over';
+    }
+}
+
+// Current Player Display Function
+const playerChange = () => {
+    if (currentPlayer % 2 != 0) {
+        currentPlayer = 1;
+    } else {
+        currentPlayer = 2;
+    }
+    currentPlayDisplay.innerText = `Player ${currentPlayer}'s Turn`;
+    currentPlayer++;
+}
 
 // Creating the game board
-for(let i = 0; i < count; i++) {
+for (let i = 0; i < count; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
     row.id = 'row' + row_id;
     row_id++;
-    for(let j = 0; j < count; j++) {
+    for (let j = 0; j < count; j++) {
         let cell = document.createElement('div');
         cell.id = cell_id;
         cell_id++;
@@ -38,4 +67,26 @@ board.addEventListener('click', () => {
 // Game reset
 reset.addEventListener('click', () => {
     location.reload();
+})
+
+// Game starts
+playerChange();
+
+// Game play logic
+document.querySelectorAll('.cell').forEach((cell) => {
+    cell.addEventListener('click', () => {
+        if (!cell.classList.contains('clicked')) {
+            playerChange();
+            playerMoves();
+            if (currentPlayer == 2) {
+                ticTacToe.splice(`${cell.id - 1}`, 1, 'O');
+                cell.innerText = 'O';
+            } else {
+                ticTacToe.splice(`${cell.id - 1}`, 1, 'X');
+                cell.innerText = 'X';
+            }
+        }
+        cell.classList.add('clicked');
+        console.log(ticTacToe);
+    })
 })
