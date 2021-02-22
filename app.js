3c3652bcc5f062e79d899bc6b00ff06ae1cb6e1c
Akshhat <akshhat.srivastava@gmail.com>
2021-02-22 10:33:05 +0530

    Add reset functionality

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const board = document.querySelector('main');
+const currentPlay = document.querySelector('#current-play');
+const reset = document.querySelector('button');
 const count = 3;
 let cell_id = 1;
 let row_id = 1;
 
+// Creating the game board
 for(let i = 0; i < count; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -18,10 +21,21 @@ for(let i = 0; i < count; i++) {
     board.appendChild(row);
 }
 
+// Adding borders to the game board
 document.querySelectorAll('.cell').forEach((cell) => {
     if (cell.id == 1 || cell.id == 4 || cell.id == 7) {
         cell.classList.add('left');
     } else if (Number(cell.id) % 3 == 0) {
         cell.classList.add('right');
     }
+})
+
+// Enabling the restart game button
+board.addEventListener('click', () => {
+    reset.disabled = false;
+})
+
+// Game reset
+reset.addEventListener('click', () => {
+    location.reload();
 })
